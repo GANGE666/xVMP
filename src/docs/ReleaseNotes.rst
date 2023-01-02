07838c262b2c2de2c64d676f91f2101c9a85a019
GANGE666 <499602203@qq.com>
2023-01-02 21:37:25 +0800

    init ollvm 8.0

---
+++ src/docs/ReleaseNotes.rst
@@ -0,0 +1,213 @@
+========================
+LLVM 8.0.0 Release Notes
+========================
+
+.. contents::
+    :local:
+
+.. warning::
+   These are in-progress notes for the upcoming LLVM 8 release.
+   Release notes for previous releases can be found on
+   `the Download Page <https://releases.llvm.org/download.html>`_.
+
+
+Introduction
+============
+
+This document contains the release notes for the LLVM Compiler Infrastructure,
+release 8.0.0.  Here we describe the status of LLVM, including major improvements
+from the previous release, improvements in various subprojects of LLVM, and
+some of the current users of the code.  All LLVM releases may be downloaded
+from the `LLVM releases web site <https://llvm.org/releases/>`_.
+
+For more information about LLVM, including information about the latest
+release, please check out the `main LLVM web site <https://llvm.org/>`_.  If you
+have questions or comments, the `LLVM Developer's Mailing List
+<https://lists.llvm.org/mailman/listinfo/llvm-dev>`_ is a good place to send
+them.
+
+Note that if you are reading this file from a Subversion checkout or the main
+LLVM web page, this document applies to the *next* release, not the current
+one.  To see the release notes for a specific release, please see the `releases
+page <https://llvm.org/releases/>`_.
+
+Non-comprehensive list of changes in this release
+=================================================
+.. NOTE
+   For small 1-3 sentence descriptions, just add an entry at the end of
+   this list. If your description won't fit comfortably in one bullet
+   point (e.g. maybe you would like to give an example of the
+   functionality, or simply have a lot to talk about), see the `NOTE` below
+   for adding a new subsection.
+
+* As `discussed on the mailing list
+  <https://lists.llvm.org/pipermail/llvm-dev/2019-January/129452.html>`_,
+  building LLVM will soon require more recent toolchains as follows:
+
+  ============= ====
+  Clang         3.5
+  Apple Clang   6.0
+  GCC           5.1
+  Visual Studio 2017
+  ============= ====
+
+  A new CMake check when configuring LLVM provides a soft-error if your
+  toolchain will become unsupported soon. You can opt out of the soft-error by
+  setting the ``LLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN`` CMake variable to
+  ``ON``.
+
+* The **llvm-cov** tool can now export lcov trace files using the
+  `-format=lcov` option of the `export` command.
+
+* The add_llvm_loadable_module CMake macro has been removed.  The
+  add_llvm_library macro with the MODULE argument now provides the same
+  functionality.  See `Writing an LLVM Pass
+  <WritingAnLLVMPass.html#setting-up-the-build-environment>`_.
+
+* For MinGW, references to data variables that might need to be imported
+  from a dll are accessed via a stub, to allow the linker to convert it to
+  a dllimport if needed.
+
+* Added support for labels as offsets in ``.reloc`` directive.
+
+.. NOTE
+   If you would like to document a larger change, then you can add a
+   subsection about it right here. You can copy the following boilerplate
+   and un-indent it (the indentation causes it to be inside this comment).
+
+   Special New Feature
+   -------------------
+
+   Makes programs 10x faster by doing Special New Thing.
+
+Changes to the LLVM IR
+----------------------
+
+
+Changes to the AArch64 Target
+-----------------------------
+
+* Added support for the ``.arch_extension`` assembler directive, just like
+  on ARM.
+
+
+Changes to the ARM Backend
+--------------------------
+
+ During this release ...
+
+
+Changes to the Hexagon Target
+-----------------------------
+
+* Added support for Hexagon/HVX V66 ISA.
+
+Changes to the MIPS Target
+--------------------------
+
+* Improved support of GlobalISel instruction selection framework.
+
+* Implemented emission of ``R_MIPS_JALR`` and ``R_MICROMIPS_JALR``
+  relocations. These relocations provide hints to a linker for optimization
+  of jumps to protected symbols.
+
+* ORC JIT has been supported for MIPS and MIPS64 architectures.
+
+* Assembler now suggests alternative MIPS instruction mnemonics when
+  an invalid one is specified.
+
+* Improved support for MIPS N32 ABI.
+
+* Added new instructions (``pll.ps``, ``plu.ps``, ``cvt.s.pu``,
+  ``cvt.s.pl``, ``cvt.ps``, ``sigrie``).
+
+* Numerous bug fixes and code cleanups.
+
+Changes to the PowerPC Target
+-----------------------------
+
+ During this release ...
+
+Changes to the X86 Target
+-------------------------
+
+* Machine model for AMD bdver2 (Piledriver) CPU was added. It is used to support
+  instruction scheduling and other instruction cost heuristics.
+
+Changes to the AMDGPU Target
+-----------------------------
+
+ During this release ...
+
+Changes to the AVR Target
+-----------------------------
+
+ During this release ...
+
+Changes to the WebAssembly Target
+---------------------------------
+
+The WebAssembly target is no longer "experimental"! It's now built by default,
+rather than needing to be enabled with LLVM_EXPERIMENTAL_TARGETS_TO_BUILD.
+
+The object file format and core C ABI are now considered stable. That said,
+the object file format has an ABI versioning capability, and one anticipated
+use for it will be to add support for returning small structs as multiple
+return values, once the underlying WebAssembly platform itself supports it.
+Additionally, multithreading support is not yet included in the stable ABI.
+
+
+Changes to the OCaml bindings
+-----------------------------
+
+
+
+Changes to the C API
+--------------------
+
+
+Changes to the DAG infrastructure
+---------------------------------
+
+External Open Source Projects Using LLVM 8
+==========================================
+
+LDC - the LLVM-based D compiler
+-------------------------------
+
+`D <http://dlang.org>`_ is a language with C-like syntax and static typing. It
+pragmatically combines efficiency, control, and modeling power, with safety and
+programmer productivity. D supports powerful concepts like Compile-Time Function
+Execution (CTFE) and Template Meta-Programming, provides an innovative approach
+to concurrency and offers many classical paradigms.
+
+`LDC <http://wiki.dlang.org/LDC>`_ uses the frontend from the reference compiler
+combined with LLVM as backend to produce efficient native code. LDC targets
+x86/x86_64 systems like Linux, OS X, FreeBSD and Windows and also Linux on ARM
+and PowerPC (32/64 bit). Ports to other architectures like AArch64 and MIPS64
+are underway.
+
+Zig Programming Language
+------------------------
+
+`Zig <https://ziglang.org>`_  is a system programming language intended to be
+an alternative to C. It provides high level features such as generics, compile
+time function execution, and partial evaluation, while exposing low level LLVM
+IR features such as aliases and intrinsics. Zig uses Clang to provide automatic
+import of .h symbols, including inline functions and simple macros. Zig uses
+LLD combined with lazily building compiler-rt to provide out-of-the-box
+cross-compiling for all supported targets.
+
+
+Additional Information
+======================
+
+A wide variety of additional information is available on the `LLVM web page
+<https://llvm.org/>`_, in particular in the `documentation
+<https://llvm.org/docs/>`_ section.  The web page also contains versions of the
+API documentation which is up-to-date with the Subversion version of the source
+code.  You can access versions of these documents specific to this release by
+going into the ``llvm/docs/`` directory in the LLVM tree.
+
+If you have any questions or comments about LLVM, please feel free to contact
+us via the `mailing lists <https://llvm.org/docs/#mailing-lists>`_.
---
+++ src/lib/Transforms/Obfuscation/StringObfuscation.cpp
@@ -0,0 +1,213 @@
+#define DEBUG_TYPE "objdiv"
+#include <string>
+#include <sstream>
+
+#include "llvm/ADT/Statistic.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Value.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Transforms/Obfuscation/CryptoUtils.h"
+#include "llvm/Transforms/Obfuscation/StringObfuscation.h"
+#include "llvm/Transforms/Obfuscation/Utils.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/Transforms/Utils/ModuleUtils.h"
+
+using namespace llvm;
+
+STATISTIC(GlobalsEncoded, "Counts number of global variables encoded");
+
+namespace llvm {
+
+        struct encVar {
+                public:
+                        GlobalVariable *var;
+                        uint8_t key;
+        };
+
+        class StringObfuscationPass: public llvm::ModulePass {
+                public:
+                static char ID; // pass identification
+                bool is_flag = false;
+                StringObfuscationPass() : ModulePass(ID) {}
+                StringObfuscationPass(bool flag) : ModulePass(ID)
+                {
+                    is_flag = flag;
+                }
+
+                virtual bool runOnModule(Module &M) {
+                        if(!is_flag)
+                            return false;
+                        std::vector<GlobalVariable*> toDelConstGlob;
+                        //std::vector<GlobalVariable*> encGlob;
+                        std::vector<encVar*> encGlob;
+                        for (Module::global_iterator gi = M.global_begin(), ge = M.global_end();
+                                                gi != ge; ++gi) {
+                                // Loop over all global variables
+                                GlobalVariable* gv = &(*gi);
+                                //errs() << "Global var " << gv->getName();
+                                std::string::size_type str_idx = gv->getName().str().find(".str.");
+                                std::string section(gv->getSection());
+
+                                // Let's encode the static ones
+                                if (gv->getName().str().substr(0,4)==".str"&&
+                                  gv->isConstant() &&
+                                gv->hasInitializer() &&
+                                isa<ConstantDataSequential>(gv->getInitializer()) &&
+                                section != "llvm.metadata" &&
+                                section.find("__objc_methname") == std::string::npos
+                              /*&&gv->getType()->getArrayElementType()->getArrayElementType()->isIntegerTy()*/) {
+                                        ++GlobalsEncoded;
+                                        //errs() << " is constant";
+
+                                        // Duplicate global variable
+                                        GlobalVariable *dynGV = new GlobalVariable(M,
+                                                                                  gv->getType()->getElementType(),
+                                                                                  !(gv->isConstant()), gv->getLinkage(),
+                                                                                  (Constant*) 0, gv->getName(),
+                                                                                  (GlobalVariable*) 0,
+                                                                                  gv->getThreadLocalMode(),
+                                                                                  gv->getType()->getAddressSpace());
+                                        // dynGV->copyAttributesFrom(gv);
+                                        dynGV->setInitializer(gv->getInitializer());
+
+                                        std::string tmp=gv->getName().str();
+                                      //  errs()<<"GV: "<<*gv<<"\n";
+
+                                        Constant *initializer = gv->getInitializer();
+                                        ConstantDataSequential *cdata = dyn_cast<ConstantDataSequential>(initializer);
+                                        if (cdata) {
+                                                const char *orig = cdata->getRawDataValues().data();
+                                                unsigned len = cdata->getNumElements()*cdata->getElementByteSize();
+
+                                                encVar *cur = new encVar();
+                                                cur->var = dynGV;
+                                                cur->key = llvm::cryptoutils->get_uint8_t();
+                                                // casting away const is undef. behavior in C++
+                                                // TODO a clean implementation would retrieve the data, generate a new constant
+                                                // set the correct type, and copy the data over.
+                                                //char *encr = new char[len];
+                                                //Constant *initnew = ConstantDataArray::getString(M.getContext(), encr, true);
+                                                char *encr = const_cast<char *>(orig);
+                                                // Simple xor encoding
+                                                for (unsigned i = 0; i != len; ++i) {
+                                                        encr[i] = orig[i]^cur->key;
+                                                }
+
+                                                // FIXME Second part of the unclean hack.
+                                                dynGV->setInitializer(initializer);
+
+                                                // Prepare to add decode function for this variable
+                                                encGlob.push_back(cur);
+                                        } else {
+                                                // just copying default initializer for now
+                                                dynGV->setInitializer(initializer);
+                                        }
+
+                                        // redirect references to new GV and remove old one
+                                        gv->replaceAllUsesWith(dynGV);
+                                        toDelConstGlob.push_back(gv);
+
+                                }
+                        }
+
+                        // actuallte delete marked globals
+                        for (unsigned i = 0, e = toDelConstGlob.size(); i != e; ++i)
+                                toDelConstGlob[i]->eraseFromParent();
+
+                        addDecodeFunction(&M, &encGlob);
+
+
+                        return true;
+                }
+
+               private:
+               void addDecodeFunction(Module *mod, std::vector<encVar*> *gvars) {
+                        // Declare and add the function definition
+                        //errs()<<"Successful enter decode function"<<"\n";
+                        std::vector<Type*>FuncTy_args;
+                        FunctionType* FuncTy = FunctionType::get(
+                          /*Result=*/Type::getVoidTy(mod->getContext()),  // returning void
+                          /*Params=*/FuncTy_args,  // taking no args
+                          /*isVarArg=*/false);
+                        uint64_t StringObfDecodeRandomName = cryptoutils->get_uint64_t();
+                        std::string  random_str;
+                        std::stringstream random_stream;
+                        random_stream << StringObfDecodeRandomName;
+                        random_stream >> random_str;
+                        StringObfDecodeRandomName++;
+                        Constant* c = mod->getOrInsertFunction(".datadiv_decode" + random_str, FuncTy);
+                        Function* fdecode = cast<Function>(c);
+                        fdecode->setCallingConv(CallingConv::C);
+
+
+                        BasicBlock* entry = BasicBlock::Create(mod->getContext(), "entry", fdecode);
+
+                        IRBuilder<> builder(mod->getContext());
+                        builder.SetInsertPoint(entry);
+
+
+                        for (unsigned i = 0, e = gvars->size(); i != e; ++i) {
+                                GlobalVariable *gvar = (*gvars)[i]->var;
+                                uint8_t key = (*gvars)[i]->key;
+
+                                Constant *init = gvar->getInitializer();
+                                ConstantDataSequential *cdata = dyn_cast<ConstantDataSequential>(init);
+
+                                unsigned len = cdata->getNumElements()*cdata->getElementByteSize();
+                                --len;
+
+                                BasicBlock *preHeaderBB=builder.GetInsertBlock();
+                                BasicBlock* for_body = BasicBlock::Create(mod->getContext(), "for-body", fdecode);
+                                BasicBlock* for_end = BasicBlock::Create(mod->getContext(), "for-end", fdecode);
+                                builder.CreateBr(for_body);
+                                builder.SetInsertPoint(for_body);
+                                PHINode *variable = builder.CreatePHI(Type::getInt32Ty(mod->getContext()), 2, "i");
+                                Value *startValue = builder.getInt32(0);
+                                Value *endValue = builder.getInt32(len);
+                                variable->addIncoming(startValue, preHeaderBB);
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+                                //LoadInst *Load=builder.CreateLoad(gvar);
+                                //errs()<<"Load: "<<*(Load->getPointerOperand())<<"\n";
+                                Value* indexList[2] = {ConstantInt::get(variable->getType(), 0), variable};
+                                Value *const_key=builder.getInt8(key);
+                                Value *GEP=builder.CreateGEP(gvar,ArrayRef<Value*>(indexList, 2),"arrayIdx");
+                                LoadInst *loadElement=builder.CreateLoad(GEP,false);
+                                loadElement->setAlignment(1);
+                                //errs()<<"Type: "<<*loadElement<<"\n";
+                                //CastInst* extended = new ZExtInst(const_key, loadElement->getType(), "extended", for_body);
+                                //Value* extended = builder.CreateZExtOrBitCast(const_key, loadElement->getType(),"extended");
+                                Value *Xor = builder.CreateXor(loadElement,const_key,"xor");
+                                StoreInst *Store = builder.CreateStore(Xor, GEP,false);
+                                Store->setAlignment(1);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                Value *stepValue = builder.getInt32(1);
+                                Value *nextValue = builder.CreateAdd(variable, stepValue, "next-value");
+                                Value *endCondition = builder.CreateICmpULT(variable, endValue, "end-condition");
+                                endCondition = builder.CreateICmpNE(endCondition, builder.getInt1(0), "loop-condition");
+                                BasicBlock *loopEndBB = builder.GetInsertBlock();
+                                builder.CreateCondBr(endCondition, loopEndBB, for_end);
+                                builder.SetInsertPoint(for_end);
+                                variable->addIncoming(nextValue, loopEndBB);
+
+                              }
+                              builder.CreateRetVoid();
+                              appendToGlobalCtors(*mod,fdecode,0);
+
+
+                }
+
+        };
+
+}
+
+char StringObfuscationPass::ID = 0;
+static RegisterPass<StringObfuscationPass> X("GVDiv", "Global variable (i.e., const char*) diversification pass", false, true);
+
+Pass * llvm::createStringObfuscation(bool flag) {
+    return new StringObfuscationPass(flag);
+}
