c8ed410ca7512c56617d561cd6ac176ac49da91e
GANGE666 <499602203@qq.com>
2023-01-02 22:15:11 +0800

    Add build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+if [ ! -d "build" ]; then
+    mkdir build
+    cd build
+    cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Release -GNinja -DCMAKE_BUILD_TYPE="Release" ../src
+else
+    cd build
+fi
+
+
+cmake --build .
+
